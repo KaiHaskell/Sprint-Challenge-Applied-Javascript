b231a5deea63cd0a24399bab11bdc9bd354817d7
kai-haskell <me@kaihaskell.com>
2019-11-01 12:34:14 -0400

    Almost Finished

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,63 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cardContainer = document.querySelector(".cards-container");
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(response => {
+    console.log(response.data.articles);
+    response.data.articles.bootstrap.forEach(i => {
+      let newArticle = ArticleCard(i.authorName, i.authorPhoto, i.headline);
+      cardContainer.appendChild(newArticle);
+    });
+    response.data.articles.javascript.forEach(i => {
+      let newArticle = ArticleCard(i.authorName, i.authorPhoto, i.headline);
+      cardContainer.appendChild(newArticle);
+    });
+    response.data.articles.tecnhology.forEach(i => {
+      let newArticle = ArticleCard(i.authorName, i.authorPhoto, i.headline);
+      cardContainer.appendChild(newArticle);
+    });
+    response.data.articles.jquery.forEach(i => {
+      let newArticle = ArticleCard(i.authorName, i.authorPhoto, i.headline);
+      cardContainer.appendChild(newArticle);
+    });
+    response.data.articles.node.forEach(i => {
+      let newArticle = ArticleCard(i.authorName, i.authorPhoto, i.headline);
+      cardContainer.appendChild(newArticle);
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+function ArticleCard(authorName, authorPhoto, headline) {
+  //creating the elements
+  const card = document.createElement("div");
+  const headlineContent = document.createElement("div");
+  const author = document.createElement("div");
+  const imgContainer = document.createElement("div");
+  const imgSrc = document.createElement("img");
+  const byAuthor = document.createElement("span");
+
+  //assigning classes to the elements
+  card.classList.add("card");
+  headlineContent.classList.add("headline");
+  author.classList.add("author");
+  imgContainer.classList.add("img-container");
+
+  //appending the elements
+  card.appendChild(headlineContent);
+  card.appendChild(author);
+  author.appendChild(imgContainer);
+  imgContainer.appendChild(imgSrc);
+  author.appendChild(byAuthor);
+
+  //adding textContent
+  headlineContent.textContent = headline;
+  author.textContent = authorName;
+  imgSrc.src = authorPhoto;
+  byAuthor.textContent = `By: ${authorName}`;
+}
