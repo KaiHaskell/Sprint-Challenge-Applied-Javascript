a5feb7303f7a7e5add4bfdea5ee754f289c329d9
kai-haskell <me@kaihaskell.com>
2019-11-01 11:21:32 -0400

    Finished index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,27 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+  //creating my elements
+
+  const headerDiv = document.createElement("div");
+  const date = document.createElement("span");
+  const headerH1 = document.createElement("h1");
+  const temperature = document.createElement("span");
+
+  //Adding classes
+  headerDiv.classList.add("header");
+  date.classList.add("date");
+  temperature.classList.add("temp");
+
+  //appending my elements
+
+  headerDiv.appendChild(date);
+  headerDiv.appendChild(headerH1);
+  headerDiv.appendChild(temperature);
+
+  return headerDiv;
+}
+const headerContainer = document.querySelector(".header-container");
+
+headerContainer.appendChild(Header());
