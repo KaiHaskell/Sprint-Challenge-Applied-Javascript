2325ac18b50cefe8ffd3f1545707ddd6cd7216ef
kai-haskell <me@kaihaskell.com>
2019-11-01 11:39:52 -0400

    Finished Tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,30 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+//creating a var for .topics
+const topicContainer = document.querySelector(".topics");
+
+//pulling from the api
+axios
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then(response => {
+    response.data.topics.forEach(topic => {
+      let tab = tabMaker(topic);
+      topicContainer.appendChild(tab);
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+//creating function
+function tabMaker(topic) {
+  const newTab = document.createElement("div");
+
+  newTab.classList.add("tab");
+
+  newTab.textContent = topic;
+
+  return newTab;
+}
